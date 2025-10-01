2d965d6a7d917c14996df0c29d31c6724d8adb34
Josh Garber <josh@jkgarber.com>
2025-10-01 11:13:38 +0200

    skip .swp files in build process

--- src/gencontent.py
+++ src/gencontent.py
@@ -43,12 +43,13 @@ def generate_page(from_path, template_path, dest_path, basepath):
 def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     files = os.listdir(dir_path_content)
     for file in files:
-        from_path = os.path.join(dir_path_content, file)
-        dest_path = os.path.join(dest_dir_path, file)
-        if os.path.isfile(from_path):
-            dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path, basepath)
-            continue
-        generate_pages_recursive(from_path, template_path, dest_path, basepath)
+        if Path(file).suffix != ".swp":
+            from_path = os.path.join(dir_path_content, file)
+            dest_path = os.path.join(dest_dir_path, file)
+            if os.path.isfile(from_path):
+                dest_path = Path(dest_path).with_suffix(".html")
+                generate_page(from_path, template_path, dest_path, basepath)
+                continue
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
 
